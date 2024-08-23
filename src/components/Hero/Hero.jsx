eaebc3cdb90c199c26f27f3ed4f0a5fd40d4a900
Charan Tejas Parimi <parimicharantejas@gmail.com>
2024-08-23 15:56:51 +0530

    updated skills and projects

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,18 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 
-import styles from './Hero.module.css'
-import {getImageUrl} from '../../utils'
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
 const Hero = () => {
-  const [displayText, setDisplayText] = useState('');
+  const [displayText, setDisplayText] = useState("");
 
   useEffect(() => {
-    const text = 'Chharan Tejas';
+    const text = "Chharan Tejas";
     const interval = 100; // Interval between each character animation in milliseconds
 
     let currentIndex = 0;
 
     const timer = setInterval(() => {
-      if (currentIndex === text.length-1) {
+      if (currentIndex === text.length - 1) {
         clearInterval(timer);
       } else {
         setDisplayText((prevText) => prevText + text[currentIndex]);
@@ -23,31 +23,42 @@ const Hero = () => {
     return () => clearInterval(timer);
   }, []);
   return (
-    <section id="home"className={styles.container}>
-        <div className={styles.content}>
-            <h1 className={styles.title}>Hi, I am 
-              <p className={styles.writtingText}>{displayText}</p></h1>
-            <p className={styles.Description}>Aspiring Full Stack Developer</p>
-            <div className={styles.side}>
-            <a href='mailto:parimicharantejas@gmail.com' className={styles.contactBtn}>Contact Me</a>
-            <a href='https://drive.google.com/file/d/1wcHdRQDsLwXc2Ce62sw1lr-q8ZhqSX5q/view?usp=sharing' className={styles.contactBtn}>CV 
-            <span class="material-symbols-outlined">
-download
-</span>
-
-            </a>
-            </div>
+    <section id="home" className={styles.container}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>
+          Hi, I am
+          <p className={styles.writtingText}>{displayText}</p>
+        </h1>
+        <p className={styles.Description}>Aspiring Full Stack Developer</p>
+        <div className={styles.side}>
+          <a
+            href="mailto:parimicharantejas@gmail.com"
+            className={styles.contactBtn}
+          >
+            Contact Me
+          </a>
+          <a
+            href="https://drive.google.com/file/d/1zj51b9K-LxPcsltQWJIsA5bhfqg8tSD7/view?usp=sharing"
+            className={styles.contactBtn}
+          >
+            CV
+            <span class="material-symbols-outlined">download</span>
+          </a>
         </div>
-        <div className={styles.box}>
+      </div>
+      <div className={styles.box}>
         <div>
-          
-            <img src={getImageUrl("hero/profilepic.jpg")} alt="hero-image" className={styles.heroImg}/>
-            <div className='{styles.topBlur}'></div>
-            <div className='{styles.bottomBlur}'></div>
-            </div>
+          <img
+            src={getImageUrl("hero/profilepic.jpg")}
+            alt="hero-image"
+            className={styles.heroImg}
+          />
+          <div className="{styles.topBlur}"></div>
+          <div className="{styles.bottomBlur}"></div>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
